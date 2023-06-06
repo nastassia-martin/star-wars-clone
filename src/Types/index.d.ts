0fdfd91f6cfac690f2fc38a95a7c3652f0017bd9
nastassia martin <nastassia.martin@googlemail.com>
2023-06-06 22:09:20 +0200

    films rendered out to DOM... the virtual DOM

--- src/Types/index.d.ts
+++ src/Types/index.d.ts
@@ -1,4 +1,25 @@
 export type SW_SearchResponse = {
+    current_page: number
+    data: SW_SearchData[]
+    from: number
+    to: number
+    last_page: number
+    total: number
+}
+
+export type SW_SearchData = {
     id: number
     title: string
+    episode_id: string
+    opening_crawl: string
+    director: string
+    producer: string
+    release_date: string
+    created: string
+    edited: string
+    characters_count: number
+    planets_count: number
+    starships_count: number
+    vehicles_count: number
+    species_count: number
 }
